63c604b9ed0e94ec9c5d38ec98a0d4941dc16591
Kyle Speight <Electr_stream@outlook.com>
2025-04-09 02:52:46 -0700

    bangocat

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ import dotenv
 from dotenv import load_dotenv
 import SQLAlchemy
 
-
+# Load environment variables from .env file
+load_dotenv()
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     DATABASE_URL = os.environ.get('DATABASE_URL')
     DEBUG = os.environ.get('DEBUG')
-    
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
