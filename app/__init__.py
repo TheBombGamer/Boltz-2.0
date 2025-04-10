00af373dfc0630d7fb59d694d1cd4d02552c063b
Kyle Speight <Electr_stream@outlook.com>
2025-04-09 23:36:13 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,6 @@
-# __init__.py
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-
-    with app.app_context():
-        from . import routes
-
+    from . import routes
     return app
