583b599b540457a7d555e2d9084f7e151fc55a01
Kyle Speight <Electr_stream@outlook.com>
2025-04-09 19:25:18 -0700

    Update run.py

--- run.py
+++ run.py
@@ -5,8 +5,6 @@ from crawler.storage import init_db
 init_db()
 
 app = create_app()
-app2 = create_app()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
-    app2.run(host="0.0.0.0", port=3000, debug=True)
